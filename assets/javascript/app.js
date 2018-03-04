23148678081c63dc89c9385c26b6ea445f2db7ea
Eric Ostler <eostler13@gmail.com>
2018-03-03 22:59:53 -0700

    Finished game logic, fixed bug where final question was undefined.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,10 @@ $("#start").on("click", function () {
 $(document).on('click', '.answer-button', function (e) {
     game.clicked(e);
 })
+
+$(document).on('click', '#reset', function() {
+    game.reset();
+})
 //questions to ask object
 var questions = [{
     question: "Why were M&M's invented?",
@@ -16,7 +20,7 @@ var questions = [{
     image: "assets/images/m&m.gif",
 }, {
     question: "Which band was once the official Disney Land band?",
-    answers: ["Reel Big Fish", "Streetlight Manifest", "Suburban Legends", "The Mighty Mighty Bosstones"],
+    answers: ["Reel Big Fish", "Streetlight Manifesto", "Suburban Legends", "The Mighty Mighty Bosstones"],
     correctAnswer: "Suburban Legends",
     image: "assets/images/disney.gif",
 }, {
@@ -34,7 +38,6 @@ var questions = [{
     answers: ["The Doberman Pinscher", "The Miniature Pinscher"],
     correctAnswer: "The Miniature Pinscher",
     image: "assets/images/doge.gif",
-}, {
 }];
 //Game Object
 var game = {
@@ -43,6 +46,7 @@ var game = {
     counter: 30,
     correct: 0,
     incorrect: 0,
+    unanswered: 0,
     countdown: function () {
         game.counter--;
         $("#counter").html(game.counter);
@@ -53,7 +57,8 @@ var game = {
     },
     loadQuestion: function () {
         timer = setInterval(game.countdown, 1000);
-        $("#subwrapper").html("<h2>" + questions[game.currentQuestion].question +
+        $('#subwrapper').html("<h2>Time Remaining <span id='counter'>30 </span> Seconds</h2>");
+        $("#subwrapper").append("<h2>" + questions[game.currentQuestion].question +
             "</h2>");
         for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
             $("#subwrapper").append("<button class='answer-button' id ='button-" + i + "'data-name='" + questions[game.currentQuestion].answers[i] + "'>" + questions[game.currentQuestion].answers[i] + "</button>");
@@ -66,10 +71,23 @@ var game = {
         game.loadQuestion();
     },
     timeUp: function () {
-
+        clearInterval(timer);
+        game.unanswered++;
+        $("#subwrapper").html('<h2> OUT OF TIME! </h2>');
+        $('#subwrapper').append('<h3>The Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     results: function () {
-
+        clearInterval(timer);
+        $('#subwrapper').html("DONE!");
+        $('#subwrapper').append("<h3>Correct: " +game.correct+"</h3>");
+        $('#subwrapper').append("<h3>Incorrect: " + game.incorrect+"</h3>");
+        $('#subwrapper').append("<h3>Unanswered: " + game.unanswered+"</h3");
+        $('#subwrapper').append("<button id='reset'>RESET</button>");
     },
     clicked: function (e) {
         clearInterval(timer);
@@ -84,7 +102,7 @@ var game = {
         clearInterval(timer);
         game.correct++;
         $("#subwrapper").html('<h2> You got it right!</h2>');
-        if (game.currentQuestion == questions.lenght - 1) {
+        if (game.currentQuestion == questions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         } else {
             setTimeout(game.nextQuestion, 3 * 1000);
@@ -95,13 +113,19 @@ var game = {
         clearInterval(timer);
         game.incorrect++;
         $("#subwrapper").html('<h2> You got it wrong!</h2>');
-        if (game.currentQuestion == questions.lenght - 1) {
+        $('#subwrapper').append('<h3>The Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        if (game.currentQuestion == questions.length - 1) {
             setTimeout(game.results, 3 * 1000);
         } else {
             setTimeout(game.nextQuestion, 3 * 1000);
         }
     },
     reset: function () {
-
+        game.currentQuestion = 0;
+        game.counter = 0;
+        game.correct = 0;
+        game.incorrect = 0;
+        game.unanswered = 0;
+        game.loadQuestion();
     }
 }
